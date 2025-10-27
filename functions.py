a4df0ccbc43f3e9db7e8a1e52036e355beb3ca36
MackToons <yurquinam.8@gmail.com>
2025-10-27 01:05:25 -0300

    deficion de funciones 'principales'

---
+++ functions.py
@@ -0,0 +1,29 @@
+def ingresar():
+    inp = input()
+    return inp
+
+def fetching(cursor):
+    fetched = cursor.fetchall()
+    return fetched
+
+def mostrarUsuarios(cursor):
+    matrix = fetch(cursor)
+    for item in matrix:
+        for i in range(len(i)):
+            print(f"{item[i]}  ")
+            
+def mostrarLibro():
+    
+def mostrarPrestamos(): 
+
+def ingresarUsuario():
+    
+def ingresarLibro():
+
+def modificarUsuario():
+
+def modificarLibro():
+    
+def eliminarElemento():
+    
+def realizarPrestamo():
